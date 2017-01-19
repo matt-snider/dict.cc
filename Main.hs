76bd45736f497c12b5e65a1c84b247ac8d2e763b
Matt Snider <matt.snider@alum.utoronto.ca>
2017-01-20 00:06:30 +0100

    Initial implementation that nearly works

---
+++ Main.hs
@@ -0,0 +1,38 @@
+import Network.HTTP
+import Text.HTML.TagSoup
+
+
+dictCC :: IO ()
+dictCC = do 
+    tags <- parseTags <$> searchWord "bier"
+    let trans = 
+            tuplify $ 
+            map f $ 
+            partitions (~== "<td class=td7nl>") tags
+    putStrLn "English                       German"
+    putStrLn "========                      ======="
+    mapM print trans
+    where
+        f :: [Tag String] -> String
+        f =  unwords . 
+             map fromTagText . 
+             filter isTagText . 
+             takeWhile (~/= "</td>")
+
+
+searchWord :: String -> IO String
+searchWord w = 
+    getResponseBody =<< simpleHTTP 
+        (getRequest $ "http://www.dict.cc/?s=" ++ w)
+
+
+tuplify :: [a] -> [(a, a)]
+tuplify [] = []
+tuplify xs = 
+    let (ys, zs) = splitAt 2 xs
+    in (ys !! 0, ys !! 1) : tuplify zs
+
+
+main :: IO ()
+main = dictCC
+
