fc36881c50b56be399526d7a04b336117ae57f6e
Matt Snider <matt.snider@alum.utoronto.ca>
2017-04-20 20:58:53 +0200

    Extract options to Options.hs

--- src/Main.hs
+++ src/Main.hs
@@ -3,53 +3,9 @@ import Data.Tuple (swap)
 import Network.HTTP
 import Text.Printf
 import Text.HTML.TagSoup
-import System.Environment
-import System.Console.GetOpt
 import qualified Data.ByteString.Char8 as BS (putStr, pack)
 
--- Option definitions and defaults
-data Options = Options
-    { optReverse  :: Bool
-    , optLimit    :: Int
-    , optFromLang :: String
-    , optToLang   :: String
-    } deriving Show
-
-defaultOptions = Options
-    { optReverse  = False
-    , optLimit    = 0
-    , optFromLang = "en"
-    , optToLang   = "de"
-    }
-
-
-options :: [OptDescr (Options -> Options)]
-options =
- [ Option ['f'] ["from"]
-    (ReqArg (\f opts -> opts { optFromLang = f }) "FROM")
-    "language to translate from"
- , Option ['t'] ["to"]
-    (ReqArg (\t opts -> opts { optToLang = t }) "TO")
-    "language to translate to"
- , Option ['l'] ["limit"]
-    (ReqArg (\l opts -> opts { optLimit = read l }) "LIMIT")
-    "limit the number of results"
- , Option ['r'] ["reverse"]
-    (NoArg (\opts -> opts { optReverse = True }))
-    "reverse the default from & to"
- ]
-
-
-getCliOpts :: IO (Options, String)
-getCliOpts = do
-    args <- getArgs
-    case getOpt Permute options args of
-        (o, w:[], []) -> return (foldl (flip id) defaultOptions o, w)
-        (o, [], [])   -> ioError (userError ("\nprovide a word to lookup\n"))
-        (_, w:ws, []) -> ioError (userError ("\nprovide only a single word to lookup\n\t"
-                                              ++ (show $ length (w:ws)) ++ " found: " ++ show (w:ws)  ++ "\n"))
-        (_,_,errs)    -> ioError (userError ("\n" ++ concat errs ++ usageInfo header options ++ "\n"))
-    where header = "Usage: dict.cc [OPTION...] word"
+import Options
 
 
 -- Main logic
---
+++ src/Options.hs
@@ -0,0 +1,54 @@
+module Options
+    (
+      Options(..)
+    , getCliOpts
+    ) where
+
+import System.Console.GetOpt
+import System.Environment
+
+
+-- Option definitions and defaults
+data Options = Options
+    { optReverse  :: Bool
+    , optLimit    :: Int
+    , optFromLang :: String
+    , optToLang   :: String
+    } deriving Show
+
+defaultOptions = Options
+    { optReverse  = False
+    , optLimit    = 0
+    , optFromLang = "en"
+    , optToLang   = "de"
+    }
+
+
+-- CLI stuff
+options :: [OptDescr (Options -> Options)]
+options =
+ [ Option ['f'] ["from"]
+    (ReqArg (\f opts -> opts { optFromLang = f }) "FROM")
+    "language to translate from"
+ , Option ['t'] ["to"]
+    (ReqArg (\t opts -> opts { optToLang = t }) "TO")
+    "language to translate to"
+ , Option ['l'] ["limit"]
+    (ReqArg (\l opts -> opts { optLimit = read l }) "LIMIT")
+    "limit the number of results"
+ , Option ['r'] ["reverse"]
+    (NoArg (\opts -> opts { optReverse = True }))
+    "reverse the default from & to"
+ ]
+
+
+getCliOpts :: IO (Options, String)
+getCliOpts = do
+    args <- getArgs
+    case getOpt Permute options args of
+        (o, w:[], []) -> return (foldl (flip id) defaultOptions o, w)
+        (o, [], [])   -> ioError (userError ("\nprovide a word to lookup\n"))
+        (_, w:ws, []) -> ioError (userError ("\nprovide only a single word to lookup\n\t"
+                                              ++ (show $ length (w:ws)) ++ " found: " ++ show (w:ws)  ++ "\n"))
+        (_,_,errs)    -> ioError (userError ("\n" ++ concat errs ++ usageInfo header options ++ "\n"))
+    where header = "Usage: dict.cc [OPTION...] word"
